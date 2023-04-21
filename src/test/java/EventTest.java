062f9b6e3c75b872797b61c4e4b80e7ee2f4379d
Milidoni Cristian <cmilidoni@fi.uba.ar>
2023-04-21 19:29:26 -0300

    Termine los test de event

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -2,88 +2,152 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import static org.junit.Assert.*;
 
 public class EventTest {
 
     @Test
-    public void Event(){
-        //Casos de equivalencia:
-        //Un evento completeDay y uno con endDate == startDate.plusHours(24) deben ser lo mismo
-
-        // arrange and act
-        LocalDateTime date1 = LocalDateTime.of(2023,4,21,10,0);
-        LocalDateTime date2 = date1.plusHours(24);
-        LocalDateTime date3 =LocalDateTime.of(2023,4,22,10,0);
-        var event1 = new Event("test1", "test1",date1,date2, false);
-        var event2 = new Event("test2", "test2",date1, null, true);
-        var event2EndDate = event2.getEndDate();
-
-
-        //assert
-        assertEquals(event2EndDate, date3);
-
-    }
-
-    @Test
-    public void repeatEvent(){
-        //Clases de equivalencia
-        //El nuevo evento mantiene entre sus startDate y endDate la misma duracion que el otro
-        //El nuevo evento creado a partir de un completeDay event debe tener entre su startDate
-        //y endDate 24hs
-
+    public void createEventWithTwoDates(){
         //arrange
         LocalDateTime date1 = LocalDateTime.of(2023,4,21,10,0);
-        LocalDateTime date2 = date1.plusHours(24);
-        LocalDateTime date3 =LocalDateTime.of(2019,7,19,1,0);
-        var event1 = new Event("test1", "test1",date1,date2, false);
-        var event2 = new Event("test2", "test2",date1, null, true);
-        var event2EndDate = event2.getEndDate();
-        var minutesExpected = 24*60;
+        LocalDateTime date2 =LocalDateTime.of(2023,5,22,10,0);
 
         //act
-        var eventRepetition1 = event1.repeatEvent(date3);
-        var eventRepetition2 = event2.repeatEvent(date2);
+        var event = new Event("test1", "test2", date1, date2, false);
+        var eventStartDate = event.getStartDate();
+        var eventEndDate = event.getEndDate();
 
-        var event1Duration = event1.getDuration();
-        var eventRepetition1Duration = eventRepetition1.getDuration();
-        var eventRepetition2Duration = eventRepetition2.getDuration();
 
         //assert
-        assertEquals(event1Duration, eventRepetition1Duration);
-        assertEquals(minutesExpected, eventRepetition2Duration);
+        assertEquals(eventStartDate, date1);
+        assertEquals(eventEndDate, date2);
+
     }
 
     @Test
-    public void getDuration(){
-        //Clases de equivalencia
-        //Un evento complete day debe devolver siempre 1440
-        //Un evento creado a partir de las dos fechas debe devolver el tiempo correcto
-        //tanto si son fechas muy alejadas como no
-
+    public void createEventWithCompleteDay(){
         //arrange
-        var date1 = LocalDateTime.of(2023,4,21,10,30);
-        var date2 = LocalDateTime.of(2023,4,21,12,29);
-        var date3 = LocalDateTime.of(2023,4,23,10,30);
-        var event1 = new Event("test1","test1",date1,date2, false);
-        var event2 = new Event("test2","test2",date1,null, true);
-        var event3 = new Event("test1","test2",date1,date3, false);
-        int expectedDuration1 = 119;
-        int expectedDuration2 = 24*60;
-        int expectedDuration3 = 2*24*60;
+        LocalDateTime date1 = LocalDateTime.of(2023,4,21,10,0);
 
-        //
-        int duration1 = event1.getDuration();
-        int duration2 = event2.getDuration();
-        int duration3 = event3.getDuration();
+        //act
+        var event1 = new Event("test1", "test1",date1, null, true);
+        var event1EndDate = event1.getEndDate();
 
-        //
-        assertEquals(expectedDuration1, duration1);
-        assertEquals(expectedDuration2, duration2);
-        assertEquals(expectedDuration3, duration3);
-
-
+        //assert
+        assertEquals(event1EndDate, date1.plusDays(1));
     }
 
+    @Test
+    public void repeatTestHasTheCorrectDates(){
+        //Arrange
+        LocalDateTime date1 =LocalDateTime.of(2019,7,19,1,0);
+        var event1 = new Event("test1", "test1", date1, null, true);
+        LocalDateTime date2 = LocalDateTime.of(2019,7,27,1,0);
+        LocalDateTime date3 = date2.plusDays(1);
+
+        //act
+        var event2 = event1.repeatEvent(date2);
+        var event2StartDate = event2.getStartDate();
+        var event2EndDate = event2.getEndDate();
+
+        //assert
+        assertEquals(date2, event2StartDate);
+        assertEquals(date3, event2EndDate);
+    }
+
+
+    @Test
+    public void repeatTestFromACompleteDayEvent(){
+        //arrange
+        LocalDateTime date3 = LocalDateTime.of(2023,4,21,10,0);
+        LocalDateTime date2 = date3.plusHours(24);
+        LocalDateTime date1 =LocalDateTime.of(2019,7,19,1,0);
+        var event1 = new Event("test1", "test1",date1, null, true);
+        var event1Duration = event1.getDuration();
+
+        //act
+        var event2 = event1.repeatEvent(date3);
+        var event2Duration = date3.until(event2.getEndDate(), ChronoUnit.MINUTES);
+
+        //assert
+        assertEquals(event1Duration, event2Duration);
+    }
+
+    @Test
+    public void repeatTestFromANotCompleteDayEvent(){
+        //arrange
+        LocalDateTime date3 = LocalDateTime.of(2023,4,21,10,0);
+        LocalDateTime date2 = date3.plusHours(24);
+        LocalDateTime date1 =LocalDateTime.of(2019,7,19,1,0);
+        var event1 = new Event("test1", "test1",date1, date2, true);
+        var event1Duration = event1.getDuration();
+
+        //act
+        var event2 = event1.repeatEvent(date3);
+        var event2Duration = date3.until(event2.getEndDate(), ChronoUnit.MINUTES);
+
+        //assert
+        assertEquals(event1Duration, event2Duration);
+    }
+
+    @Test
+    public void completeDayEventDurationIs1440(){
+        //assert
+        var date1 = LocalDateTime.of(2023,4,21,10,30);
+        var event1 = new Event("test1","test1",date1,null, true);
+        int expectedDuration = 1440;
+
+        //act
+        var event1Duration= event1.getDuration();
+
+        //assert
+        assertEquals(event1Duration,expectedDuration);
+    }
+
+    @Test
+    public void eventDurationWithinDays(){
+        //arrage
+        var date1 = LocalDateTime.of(2023,4,21,10,30);
+        var date2 = LocalDateTime.of(2023,4,24,10,30);
+        var event1 = new Event("test1","test1",date1, date2, false);
+        int expectedDuration = 3*60*24;
+
+        //act
+        var event1Duration = event1.getDuration();
+
+        //assert
+        assertEquals(event1Duration, expectedDuration);
+    }
+
+    @Test
+    public void eventDurationWithinWeeks(){
+        //arrage
+        var date1 = LocalDateTime.of(2023,4,10,10,30);
+        var date2 = LocalDateTime.of(2023,4,24,10,30);
+        var event1 = new Event("test1","test1",date1, date2, false);
+        int expectedDuration = 2*7*24*60;
+
+        //act
+        var event1Duration = event1.getDuration();
+
+        //assert
+        assertEquals(event1Duration, expectedDuration);
+    }
+
+    @Test
+    public void eventDurationWithinMonths(){
+        //arrage
+        var date1 = LocalDateTime.of(2023,4,10,10,30);
+        var date2 = LocalDateTime.of(2023,5,10,10,30);
+        var event1 = new Event("test1","test1",date1, date2, false);
+        int expectedDuration = (int)date1.until(date2, ChronoUnit.MINUTES);
+
+        //act
+        var event1Duration = event1.getDuration();
+
+        //assert
+        assertEquals(event1Duration, expectedDuration);
+    }
 }
